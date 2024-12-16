f631817d05ea05d5442f30db2aa8dc5b6e05f1a2
zubrein <robi3443@gmail.com>
2024-12-16 18:40:19 +0600

    Add firebase realtime database template

--- lib/controllers/contact_controller.dart
+++ lib/controllers/contact_controller.dart
@@ -1,11 +1,8 @@
 import 'package:get/get.dart';
 
 import '../models/contact.dart';
-import '../service/firebase_service.dart';
 
 class ContactController extends GetxController {
-  final FirebaseService _firebaseHelper = FirebaseService();
-
   // Observables
   var contactList = <Contact>[].obs;
 
@@ -16,48 +13,14 @@ class ContactController extends GetxController {
   }
 
   // Fetch contacts using FirebaseHelper
-  void fetchContacts() {
-    _firebaseHelper.getContactsRef().onValue.listen((event) {
-      final data = event.snapshot.value as Map<dynamic, dynamic>?;
-
-      if (data != null) {
-        contactList.value = data.entries.map((entry) {
-          return Contact.fromJson(
-              entry.key, Map<String, dynamic>.from(entry.value));
-        }).toList();
-      } else {
-        contactList.clear();
-      }
-    });
-  }
+  void fetchContacts() {}
 
   // Add a new contact
-  Future<void> addContact(Contact contact) async {
-    try {
-      await _firebaseHelper.addContact(contact);
-      Get.snackbar('Success', 'Contact added successfully');
-    } catch (e) {
-      Get.snackbar('Error', 'Failed to add contact: $e');
-    }
-  }
+  Future<void> addContact(Contact contact) async {}
 
   // Update an existing contact
-  Future<void> updateContact(Contact contact) async {
-    try {
-      await _firebaseHelper.updateContact(contact);
-      Get.snackbar('Success', 'Contact updated successfully');
-    } catch (e) {
-      Get.snackbar('Error', 'Failed to update contact: $e');
-    }
-  }
+  Future<void> updateContact(Contact contact) async {}
 
   // Delete a contact
-  Future<void> deleteContact(String id) async {
-    try {
-      await _firebaseHelper.deleteContact(id);
-      Get.snackbar('Success', 'Contact deleted successfully');
-    } catch (e) {
-      Get.snackbar('Error', 'Failed to delete contact: $e');
-    }
-  }
+  Future<void> deleteContact(String id) async {}
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
